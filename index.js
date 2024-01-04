3c68a278aedf8348cbfb0a25f8212f1df38854d3
fcrbr <filipecr@gmail.com>
2024-01-04 16:51:39 -0300

    coloquei time out

--- index.js
+++ index.js
@@ -5,23 +5,38 @@ var io = require("socket.io")(http);
 
 io.on("connection",(socket) => {
 
-    socket.on("disconnect", () => {
-        console.log("x desconectou: " + socket.id);
-    })
+  socket.on("disconnect", () => {
+    console.log("Desconectado");
+  });
 
-    socket.on("msg", (data) => {
-        io.emit("showmsg",data);
-        console.log(data);
-    });
- 
+
+  socket.on("showmsg", (data) => {
+    console.log(data);
+    var chat = document.getElementById("chat");
+    var p = document.createElement("p");
+    p.innerHTML = data.username + ": " + data.msg;
+    chat.append(p);
+
+  });
+
+  function enviar(){
+    var msgField = document.getElementById("msg");
+    var usernameField = document.getElementById("username");
+
+    var msg = msgField.value;
+    var username = usernameField.value;
+
+    socket.emit("msg", {msg: msg, username: username}, { timeout: 5000 }); // Timeout after 5 seconds
+
+  }
 });
 
 app.set("view engine","ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
 
 http.listen(3000, () => {
-    console.log("APP RODANDO");
-})
+  console.log("APP RODANDO");
+});
